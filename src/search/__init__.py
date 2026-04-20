16142a49ccc36b7bd98724273af475e0b3be2340
Mohammed Misran <mohammed.misran38@gmail.com>
2026-04-19 23:38:01 -0400

    feat(search): export TermFilter from search module

--- src/search/__init__.py
+++ src/search/__init__.py
@@ -1,6 +1,13 @@
 """Search and query expansion components."""
 
 from src.search.beam_search import BeamSearchExpander
-from src.search.tfidf_retriever import TFIDFRetriever, save_model, load_model
+from src.search.term_filter import TermFilter
+from src.search.tfidf_retriever import TFIDFRetriever, load_model, save_model
 
-__all__ = ["BeamSearchExpander", "TFIDFRetriever", "save_model", "load_model"]
+__all__ = [
+    "BeamSearchExpander",
+    "TermFilter",
+    "TFIDFRetriever",
+    "load_model",
+    "save_model",
+]
