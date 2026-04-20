4760bfca446a0fb9a524615257c7c86fe02faec8
Mohammed Misran <mohammed.misran38@gmail.com>
2026-04-19 21:13:47 -0400

    feat(search): export public API from search module

--- src/search/__init__.py
+++ src/search/__init__.py
@@ -1,1 +1,6 @@
 """Search and query expansion components."""
+
+from src.search.beam_search import BeamSearchExpander
+from src.search.tfidf_retriever import TFIDFRetriever, save_model, load_model
+
+__all__ = ["BeamSearchExpander", "TFIDFRetriever", "save_model", "load_model"]
