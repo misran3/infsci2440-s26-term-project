5ad76691076cddf5b0c9ab867ab24e6b1156fbfc
Mohammed Misran <mohammed.misran38@gmail.com>
2026-04-19 22:47:24 -0400

    test(judge): add term relevance evaluation test

---
+++ tests/judge/test_term_relevance.py
@@ -0,0 +1,30 @@
+"""Term relevance evaluation tests."""
+
+import pytest
+
+from src.judge.heuristics import check_expansion_not_empty
+from src.judge.llm_judge import evaluate_term_relevance
+from src.judge.models import TermRelevanceTest
+from src.search import BeamSearchExpander
+
+QUERIES = ["bug", "crash", "good", "easy"]
+
+
+@pytest.mark.asyncio
+async def test_term_relevance(judge_collector):
+    """Evaluate term relevance for common queries."""
+    expander = BeamSearchExpander(beam_width=3, max_depth=2)
+
+    for query in QUERIES:
+        result = expander.expand(query)
+
+        heuristic = check_expansion_not_empty(query, result.expanded_terms)
+        judge_collector.add_test(heuristic)
+
+        judgment = await evaluate_term_relevance(query, result.expanded_terms)
+        test_result = TermRelevanceTest(
+            query=query,
+            expanded_terms=result.expanded_terms,
+            judgments=judgment.judgments,
+        )
+        judge_collector.add_test(test_result)
